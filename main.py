b6b3479cb84c22752605ab07863f1e27e5bb0e5f
mataeo-eh <mataeo.eh@gmail.com>
2025-07-17 17:30:24 -0500

    Cleaned up main.py code, removed comments, improved the exception handling, added a main guard

--- main.py
+++ main.py
@@ -3,28 +3,24 @@ from stats import char_count
 from stats import sorted_dictionaries_list
 import sys
 
-
-#def generic_use():
-
-
-
-def main(path):
+def main():
+    if len(sys.argv) != 2:
+        raise Exception("Usage: python3 main.py <path_to_book>")
+    path = sys.argv[1]
     print ("============ BOOKBOT ============")
     print (f"Analyzing book found at {path}")
     print ("----------- Word Count ----------")
     print (f"Found {count_words(path)} total words")
     print ("--------- Character Count -------")
     for char_dict in sorted_dictionaries_list(path):
-        print (f"{char_dict["char"]}: {char_dict["num"]}")
+        print (f"{char_dict['char']}: {char_dict['num']}")
     print ("============= END ===============")
-
     return 
-    
-if len(sys.argv) <= 1:
-    print ("Usage: python3 main.py <path_to_book>")
-    sys.exit(1)
-else:
-    path = sys.argv[1]
-    print (main(path))
 
-# main ("books/frankenstein.txt") #Made a comment just for testing purposes
+
+if __name__ == "__main__":
+    try:
+        main()
+    except Exception as input_error:
+        print (input_error)
+        sys.exit(1)
