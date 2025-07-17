49b68a757171661adf7adb71fd1792874c91aa9a
mataeo-eh <mataeo.eh@gmail.com>
2025-07-17 16:16:59 -0500

    Added cleaner output to the bookbot. Organized character output from most to least. Changed output to only include .alpha=true

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 from stats import count_words
 from stats import char_count
+from stats import sorted_dictionaries_list
+
 
 def main(path):
-    print (count_words(path), char_count(path))
-    return (count_words(path), char_count(path))
+    print ("============ BOOKBOT ============")
+    print (f"Analyzing book found at {path}")
+    print ("----------- Word Count ----------")
+    print (f"Found {count_words(path)} total words")
+    print ("--------- Character Count -------")
+    for char_dict in sorted_dictionaries_list(path):
+        print (f"{char_dict["char"]}: {char_dict["num"]}")
+    print ("============= END ===============")
+
+    return 
+    
+
 
 main ("books/frankenstein.txt")
--- stats.py
+++ stats.py
@@ -1,9 +1,12 @@
+
+
 def count_words(path):
     text= get_book_text(path)
     word_count= text.split() 
     final_count = len(word_count)
-    return f"{final_count} words found in the document"
-    
+    # return f"{final_count} words found in the document"
+    # print (final_count)
+    return final_count
 
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
@@ -20,4 +23,37 @@ def char_count(path):
             characters_dictionary[character] += 1
     return characters_dictionary
 
+def sorting_key(to_sort):
+    return to_sort["num"]
 
+
+def sorted_dictionaries_list(path):
+    dictionary = char_count(path)
+    new_dics_list = []
+    analyzed_count = []
+    for character in dictionary:
+        number = dictionary[character]
+        if character.isalpha() == True:
+            new_dics_list.append({"char": character, "num": number})
+            new_dics_list.sort(reverse=True, key=sorting_key)
+    # Below used for testing purposes
+    # print (new_dics_list)
+    # return new_dics_list[1]["char"], new_dics_list[1]["num"]
+    # for dictionary in new_dics_list:
+        #analyzed_count.append(f"{dictionary["char"]}: {dictionary["num"]}")
+    # analyzed_count_string = " ".join(analyzed_count)
+       # print (f"{dictionary["char"]}: {dictionary["num"]}")
+    return new_dics_list
+    
+
+
+# used for testing, kept to use as a reference.
+# ef main (path):
+   # print (sorted_dictionaries_list(path))
+   # print (char_count(path))
+   # print (sorted_dictionaries_list(path)) 
+
+
+
+# See above.
+# main("books/frankenstein.txt")
